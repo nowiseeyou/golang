c056a20d1cfb39d46e6d87c8c673f50ed7d77b94
Willian <ug@rd.com>
2019-08-20 09:52:54 +0800

    指针变量

---
+++ secondWeek/addressip.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+    var a int = 20 //声明实际变量
+    var ip *int //声明指针变量
+
+    ip = &a //指正变量存储地址
+
+    fmt.Printf("a 变量地址是：%x\n",&a)
+
+    fmt.Printf("ip 变量储存的指针地址：%x\n",ip)
+
+    //ip变量的指针访问值
+    fmt.Printf("ip 变量的值：%d\n",*ip)
+}
