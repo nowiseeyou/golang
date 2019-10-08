cf5b675f9c038b9802c16653963a1c9ab5fc9edf
Willian <ug@rd.com>
2019-10-08 09:14:04 +0800

    panic 输出报错

--- courseNotes/grade.go
+++ courseNotes/grade.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	fmt.Println("Hello")
-	grade(101)
-	//fmt.Printf(grade(101))
+	fmt.Println(returnStr())
+	//grade(101)
+	fmt.Println(grade(101))
 
 }
 
@@ -15,17 +16,22 @@ func main() {
 func grade(score int) string {
 
 	g := ""
-	switch  {
-	case  score < 0 || score >100:
-		panic(fmt.Sprintf("Wrong score: %d",score))
-	case  score < 60:
-		g = "D"
-	case  score < 80:
-		g = "C"
-	case  score < 90:
-		g = "B"
-	case  score < 100:
-		g = "A"
+	switch {
+		case  score < 0 || score >100:
+			g = "金额错误，超出0-100的范围：" + string(score)
+		case  score < 60:
+			g = "D"
+		case  score < 80:
+			g = "C"
+		case  score < 90:
+			g = "B"
+		case  score < 100:
+			g = "A"
  	}
 	return g
+}
+
+//函数返回
+func returnStr () string{
+	return "ig np"
 }
