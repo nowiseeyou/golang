bc5c4bf8909b7f56c4d3c03c600099538980a9d7
Willian <ug@rd.com>
2019-10-23 12:30:35 +0800

    treeNode

--- courseNotes/面向对象/treeNode.go
+++ courseNotes/面向对象/treeNode.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+
+type treeNode struct {
+	value int
+	left,right *treeNode
+}
+
+//工厂函数
+func createNode(value int) *treeNode {
+	return &treeNode{value:value}
+}
+
 func main(){
 	fmt.Println()
+	
+	root := treeNode{
+		value: 3,
+		left:  nil,
+		right: nil,
+	}
+
+	root.left = &treeNode{}
+	root.left.right = createNode(2)
+	root.right = &treeNode{5,nil,nil}
+	root.right.left = new(treeNode)
+
+	nodes := []treeNode{
+		{},
+		{},
+		{6,nil,&root},
+	}
+	fmt.Println(nodes)
+	fmt.Println(root)
 }
+
