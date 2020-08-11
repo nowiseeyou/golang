f8beee9443e013f3acf195dc3c40ece0f20df3fc
nowiseeyou <1182801311@qq.com>
2020-08-11 10:40:31 +0800

    格式化代码

--- courseNotes/2-基础语法/grade.go
+++ courseNotes/2-基础语法/grade.go
@@ -7,32 +7,32 @@ import (
 func main() {
 	fmt.Println("Hello")
 	fmt.Println(returnStr())
-	//grade(101)
+	// grade(101)
 	fmt.Println(grade(101))
-
+	
 }
 
-//switch
+// switch
 func grade(score int) string {
-
+	
 	g := ""
 	switch {
-		case  score < 0 || score >100:
-			//panic(fmt.Sprintf("Wrong score %d",score))
-			g = "金额错误，超出0-100的范围：" + string(score)
-		case  score < 60:
-			g = "D"
-		case  score < 80:
-			g = "C"
-		case  score < 90:
-			g = "B"
-		case  score < 100:
-			g = "A"
- 	}
+	case score < 0 || score > 100:
+		// panic(fmt.Sprintf("Wrong score %d",score))
+		g = "金额错误，超出0-100的范围：" + string(score)
+	case score < 60:
+		g = "D"
+	case score < 80:
+		g = "C"
+	case score < 90:
+		g = "B"
+	case score < 100:
+		g = "A"
+	}
 	return g
 }
 
-//函数返回
-func returnStr () string{
+// 函数返回
+func returnStr() string {
 	return "ig np"
 }
