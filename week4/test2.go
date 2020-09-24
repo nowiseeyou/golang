c738c46167b037cd275fe35afb8db8cb19b655b8
nowiseeyou <1182801311@qq.com>
2020-09-24 19:47:34 +0800

    defer

--- week4/test2.go
+++ week4/test2.go
@@ -6,11 +6,13 @@ import (
 	"os"
 )
 
+const filename = "./week4/note.txt"
+
 // Contents 将文件的内容作为字符串返回
-func Contents (filename string) (string, error) {
+func Contents(filename string) (string, error) {
 	f, err := os.Open(filename)
 	if err != nil {
-		 return "", err
+		return "", err
 	}
 	
 	defer f.Close() // f.Close 会在我们结束之后运行
@@ -21,22 +23,25 @@ func Contents (filename string) (string, error) {
 		n, err := f.Read(buf[0:])
 		result = append(result, buf[0:n]...)
 		if err != nil {
-			if err == io.EOF{
+			if err == io.EOF {
 				break
 			}
-			return "", err  // 这里返回后，f 将会关闭
+			return "", err // 这里返回后，f 将会关闭
 		}
 	}
 	
-	return  string(result),nil // 这里返回后，f 将会关闭
+	return string(result), nil // 这里返回后，f 将会关闭
 }
 
-func main()  {
+func main() {
 	
-	content,err := Contents("note.txt")
-	fmt.Printf("content: %s ",content)
-	panic(err)
+	content, err := Contents(filename)
+	fmt.Printf("content:  %s \n ", content)
+	if err != nil {
+		panic(err)
+	}
+	
 	world := "世界"
-	fmt.Printf("hello %s" ,world)
+	fmt.Printf("hello %s \n", world)
+	
 }
-
