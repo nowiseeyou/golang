f0285a546a6deb10c0234a3fc3af75c1af0e09fc
nowiseeyou <1182801311@qq.com>
2020-09-27 18:13:06 +0800

    test

--- week4/test4.go
+++ week4/test4.go
@@ -1,2 +1,51 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"sync"
+)
+
+type SyncedBuffer struct {
+	lock sync.Mutex
+	buffer bytes.Buffer
+}
+
+// func NewFile (fd int, name string ) *File{
+// 	if fd < 0 {
+// 		return nil
+// 	}
+//
+// 	f := new (File)
+// 	f.fd = fd
+// 	f.name = name
+// 	f.dirinfo = nil
+// 	f.nepipe = 0
+// 	return f
+//
+// }
+
+func NewFile(fd int, name string ) *File {
+	if fd < 0 {
+		return nil
+	}
+	
+	// f := File{fd, name, nil, 0}
+	// return &File{fd, name, nil, 0}
+	return &File{fd: fd, name: name}
+}
+
+func main(){
+	p := new (SyncedBuffer) // type *SyncedBuffer
+	var v SyncedBuffer      // type  SyncedBuffer
+	fmt.Print(p)
+	fmt.Print(v)
+	
+	// 创建数组 切片
+	a := [...]string  {Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+	s := []string  {Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+	m := [...]string  {Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+}
+
+
