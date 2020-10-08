554493a256210de0ef90fe97e0233f7ebfdfc5aa
nowiseeyou <1182801311@qq.com>
2020-10-08 15:19:28 +0800

    马尔可夫算法

--- week6/day4-1.go
+++ week6/day4-1.go
@@ -18,32 +18,32 @@ type score struct {
 }
 
 // action 将一个动作随机换为一个分数
-type action func (current score) (result score,turnIsOver bool)
+type action func(current score) (result score, turnIsOver bool)
 
 // roll 返回模拟一次掷骰产生的 (result,turnIsOver)
 // 若掷骰的结果 outcome 为1 ，那么这一轮 thisTurn 的分数就会被抛弃
 // 然后玩家的角色互换，否则，此次掷骰的值就会计入这一轮 thisTurn 中
 
-func roll(s score) (score,bool) {
+func roll(s score) (score, bool) {
 	outcome := rand.Intn(6) + 1 // A random int in [1,6]
 	if outcome == 1 {
-		return score{s.opponent,s.player + s.thisTurn,0},true
+		return score{s.opponent, s.player + s.thisTurn, 0}, true
 	}
 	
-	return score{s.player,s.opponent,outcome + s.thisTurn},false
+	return score{s.player, s.opponent, outcome + s.thisTurn}, false
 }
 
 // stay 返回停留时产生的结果 （result,turnIsOver） outcome of staying
 // 这一轮 thisTurn 的分数会计入该玩家的总分中，然后玩家角色互换
 func stay(s score) (score, bool) {
-	return score{s.opponent,s.player + s.thisTurn,0},true
+	return score{s.opponent, s.player + s.thisTurn, 0}, true
 }
 
 // strategy 为任何给定的分数 score 返回一个动作 action
 type strategy func(score) action
 
 // strategy 返回一个策略，该策略继续掷骰直到这一轮 thisTurn 至少为 k, 然后停留
-func stayAtK(k int) strategy{
+func stayAtK(k int) strategy {
 	return func(s score) action {
 		if s.thisTurn >= k {
 			return stay
@@ -53,14 +53,14 @@ func stayAtK(k int) strategy{
 }
 
 // play 模拟一场 Pig 游戏并返回赢家（0 或 1）
-func play(strategy0,strategy1 strategy) int {
+func play(strategy0, strategy1 strategy) int {
 	strategies := []strategy{strategy0, strategy1}
 	var s score
 	var turnIsOver bool
 	
 	// 随机决定谁先玩
-	currentPlayer := rand.Intn(2)   // Randomly decide who plays first
-	for s.player + s.thisTurn < win {
+	currentPlayer := rand.Intn(2) // Randomly decide who plays first
+	for s.player+s.thisTurn < win {
 		action := strategies[currentPlayer](s)
 		s, turnIsOver = action(s)
 		if turnIsOver {
@@ -71,10 +71,10 @@ func play(strategy0,strategy1 strategy) int {
 }
 
 // RoundRobin 模拟每一对策略 strategies 之间的一系列游戏
-func roundRobin (strategies []strategy) ([]int, int) {
+func roundRobin(strategies []strategy) ([]int, int) {
 	wins := make([]int, len(strategies))
 	for i := 0; i < len(strategies); i++ {
-		for j := i+1; j < len(strategies); j++ {
+		for j := i + 1; j < len(strategies); j++ {
 			for k := 0; k < gamesPerSeries; k++ {
 				winner := play(strategies[i], strategies[j])
 				if winner == 0 {
@@ -95,13 +95,13 @@ func roundRobin (strategies []strategy) ([]int, int) {
 // 它列出了每一个值以及它对于所有值之和的百分比
 // 例如，ratios(1,2,3) = "1/6 (16.7%), 2/6 (33.3%), 3/6 (50.0%)"
 
-func ratioString (vals ...int) string {
+func ratioString(vals ...int) string {
 	total := 0
-	for _,val := range vals {
+	for _, val := range vals {
 		total += val
 	}
 	s := ""
-	for _,val := range vals {
+	for _, val := range vals {
 		if s != "" {
 			s += ", "
 		}
@@ -114,16 +114,14 @@ func ratioString (vals ...int) string {
 func main() {
 	strategies := make([]strategy, win)
 	for k := range strategies {
-		strategies[k] = stayAtK( k +1 )
+		strategies[k] = stayAtK(k + 1)
 	}
 	
-	wins,games := roundRobin(strategies)
+	wins, games := roundRobin(strategies)
 	
 	for k := range strategies {
 		fmt.Printf("Wins, losses staying at k = % 4d: %s \n",
 			k+1,
-			ratioString(wins[k], games - wins[k]))
+			ratioString(wins[k], games-wins[k]))
 	}
 }
-
-
