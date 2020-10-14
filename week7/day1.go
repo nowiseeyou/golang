f59ef92e30379576e369430f64149447a1e6ac3a
nowiseeyou <1182801311@qq.com>
2020-10-14 13:52:42 +0800

    golang 快速排序

--- week7/day1.go
+++ week7/day1.go
@@ -6,8 +6,8 @@ import "fmt"
 
 func bubbleSort(arr []int) []int {
 	len := len(arr)
-	for i := 0; i < len; i++ { // 1-4 2-3 3-2 2-1 1
-		for j := 0; j < len-1-i; j++ { // 4 3 2 1
+	for i := 0; i < len; i++ {  // 1-4 2-3 3-2 2-1 1
+		for j := 0; j < len-1-i; j++ {  // 4 3 2 1
 			// 0 1 2 3
 			// 0 1 2
 			// 0 1
@@ -20,11 +20,28 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
+
+// 快速排序
+func selectionSort(arr []int ) []int {
+	len := len(arr)
+	for i := 0; i < len - 1; i++{
+		min := i
+		for j := i+1; j<len; j++ {
+			if arr[min] > arr[j] {
+				min = j
+			}
+		}
+		arr[i], arr[min] = arr[min], arr[i]
+	}
+	
+	return arr
+}
+
 func main() {
 	arr := []int{24, 21, 6, 1, 15}
 	
-	sorArr := bubbleSort(arr)
+	bubbleSortArr := bubbleSort(arr)
 	
-	fmt.Println(sorArr)
+	fmt.Println(bubbleSortArr)
 	
 }
